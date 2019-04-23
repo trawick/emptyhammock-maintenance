cc036d08d08c6c54297e2a2692b0a5c2e5e6f9e2
Jeff Trawick <trawick@gmail.com>
2019-04-23 19:57:02 -0400

    Fix path to repo clone

--- maintain.sh
+++ maintain.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd $HOME/git/maintenance
+cd $HOME/git/emptyhammock-maintenance
 . env/bin/activate
 ./maintain.py /netstore/emptyhammock/backups /netstore/emptyhammock/logs /netstore/emptyhammock/config "$@"
